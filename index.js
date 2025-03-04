806269e99ad24914b90a90b8229d1c6ffaf99d7e
Brian <okellobrian18@gmail.com>
2025-03-04 11:41:09 +0300

    Strings

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "Grace Hopper"
+
+const welcomeMessage = `${currentUser} Welcome to Flatbook, !`
+
+// const excitedWelcomeMessage = `${currentUser.toUpperCase()} WELCOME TO FLATBOOK, !`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+// const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+// const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
